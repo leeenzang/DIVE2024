4a47996a8584166fa08d1716b5b5ee4c4bd0f213
leeeunjin <enzn@hufs.ac.kr>
2024-10-05 10:14:08 +0900

    FEAT: DB구조 변경

--- common/admin.py
+++ common/admin.py
@@ -3,5 +3,5 @@ from .models import Coastline
 
 @admin.register(Coastline)
 class CoastlineAdmin(admin.ModelAdmin):
-    list_display = ('name', 'alias_names')  # 해안 이름만 표시
-    search_fields = ['name', 'alias_names']  # 해안 이름으로 검색 가능
+    list_display = ('name', 'alias_names', 'large_area')  # large_area를 제거하고 기존 필드만 사용
+    search_fields = ['name', 'alias_names', 'large_area']
--- common/models.py
+++ common/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Coastline(models.Model):
     name = models.CharField(max_length=255)  # 해안 이름
-    alias_names = models.TextField(blank=True, null=True)  # 해안의 별칭들을 쉼표로 구분하여 저장
+    alias_names = models.CharField(max_length=255, blank=True, null=True)  # 별칭
+    large_area = models.CharField(max_length=255, blank=True, null=True)  # 큰 지역
+
     def __str__(self):
         return self.name
