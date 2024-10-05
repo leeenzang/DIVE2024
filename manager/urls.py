f2f1cbe0b23cf7b450cee20ed83df1f84c3289b5
leeeunjin <enzn@hufs.ac.kr>
2024-10-05 12:44:55 +0900

    FEAT: 기초통계 기능 추가

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import filter_data
-
+from .views import (filter_data, 
+    get_total_collection_trend,
+    get_litter_type_ratio,
+    get_predicted_vs_actual_comparison
+)
 urlpatterns = [
     path('filter-data/', filter_data, name='filter_data'),
+    path('total-collection-trend/', get_total_collection_trend, name='total_collection_trend'),
+    path('litter-type-ratio/', get_litter_type_ratio, name='litter_type_ratio'),
+    path('predicted-vs-actual-comparison/', get_predicted_vs_actual_comparison, name='predicted_vs_actual_comparison'),
+
 ]
